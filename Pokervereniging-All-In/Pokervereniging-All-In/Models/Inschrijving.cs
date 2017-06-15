5db892556e924bf98eaefa8dd5cc88ea614a3aaf
PrincePanda <ruben@marienhove.nl>
2017-06-15 11:23:00 +0200

    Added: playerdeelnameController

---
+++ Pokervereniging-All-In/Pokervereniging-All-In/Models/Inschrijving.cs
@@ -0,0 +1,34 @@
+﻿using Pokervereniging_All_In.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokervereniging_All_In.Models
+{
+    class Toernooi : Event        
+    {
+        public int Ecode { get; set; }
+        public int Mindeelnemers { get; set; }
+        public int Inleggeld { get; set; }
+        public Speler Eersteplaats { get; set; }
+        public Speler Tweedeplaats { get; set; }
+        public Speler Derdeplaats { get; set; }
+
+        public Toernooi(int e_code, int mindeelnemers, int inleggeld, Speler eersteplaats = null, Speler tweedeplaats = null, Speler derdeplaats = null )
+        {
+            Ecode = e_code;
+            Mindeelnemers = mindeelnemers;
+            Inleggeld = inleggeld;
+            Eersteplaats = eersteplaats;
+            Tweedeplaats = tweedeplaats;
+            Derdeplaats = derdeplaats;
+        }
+
+        public override string ToString()
+        {
+            return Ecode.ToString();
+        }
+    }
+}
