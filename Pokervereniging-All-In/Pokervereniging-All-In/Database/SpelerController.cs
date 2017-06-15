4c0d5fde9ddd550f45a0d71b423575d385dd3c41
PrincePanda <ruben@marienhove.nl>
2017-06-15 03:15:42 +0200

    Added: getting players orderd by rating

--- Pokervereniging-All-In/Pokervereniging-All-In/Database/SpelerController.cs
+++ Pokervereniging-All-In/Pokervereniging-All-In/Database/SpelerController.cs
@@ -103,5 +103,50 @@ namespace Pokervereniging_All_In.Database
 
             return speler;
         }
+
+        public List<Speler> GetSpelersByRating()
+        {
+            List<Speler> spelers = new List<Speler>();
+
+            try
+            {
+                conn.Open();
+
+                string selectQuery = @"SELECT * FROM speler ORDER BY rating DESC";
+                MySqlCommand cmd = new MySqlCommand(selectQuery, conn);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    int p_code = dataReader.GetInt32("p_code");
+                    string roepnaam = dataReader.GetString("roepnaam");
+                    string voorletters = dataReader.GetString("voorletters");
+                    string tussenvoegsels = dataReader[3] as string;
+                    string achternaam = dataReader.GetString("achternaam");
+                    char geslacht = dataReader.GetChar("geslacht");
+                    string postcode = dataReader.GetString("postcode");
+                    string straat = dataReader.GetString("straat");
+                    int huisnummer = dataReader.GetInt32("huisnummer");
+                    string woonplaats = dataReader.GetString("woonplaats");
+                    string emailadres = dataReader.GetString("emailadres");
+                    string IBAN_nummer = dataReader.GetString("IBAN_nummer");
+                    int rating = dataReader.GetInt32("rating");
+                    bool staat_op_blacklist = dataReader.GetChar("staat_op_blacklist") == 'J';
+
+                    Speler speler = new Speler(p_code, roepnaam, voorletters, tussenvoegsels, achternaam, geslacht, postcode, straat, huisnummer, woonplaats, emailadres, IBAN_nummer, rating, staat_op_blacklist);
+                    spelers.Add(speler);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong when trying to " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return spelers;
+        }
     }
 }
