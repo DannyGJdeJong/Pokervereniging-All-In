88bcee0118c26a5f010a34e897447e220b021758
PrincePanda <ruben@marienhove.nl>
2017-06-15 02:27:14 +0200

    Added: toernooi controller and toernooi class

---
+++ Pokervereniging-All-In/Pokervereniging-All-In/Database/ToernooiController.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using Pokervereniging_All_In.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokervereniging_All_In.Database
+{
+    class ToernooiController : DatabaseController
+    {
+        public List<Toernooi> GetToernooien()
+        {
+            List<Toernooi> Toernooien = new List<Toernooi>();
+
+            try
+            {
+                conn.Open();
+
+                string selectQuery = @"SELECT * FROM toernooi";
+                MySqlCommand cmd = new MySqlCommand(selectQuery, conn);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    int e_code = dataReader.GetInt32("e_code");
+                    int mindeelnemers = dataReader.GetInt32("minimum_deelnemers");
+                    int inleggeld = dataReader.GetInt32("inleggeld");
+                    int eersteplaats = dataReader.GetInt32("eerste_plaats");
+                    int tweedeplaats = dataReader.GetInt32("tweede_plaats");
+                    int derdeplaats = dataReader.GetInt32("derde_plaats");
+
+                    Toernooi toernooi = new Toernooi(e_code, mindeelnemers, inleggeld, eersteplaats, tweedeplaats, derdeplaats);
+                    Toernooien.Add(toernooi);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong when trying to " + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return Toernooien;
+        }
+    }
+}
---
+++ Pokervereniging-All-In/Pokervereniging-All-In/Models/Toernooi.cs
@@ -0,0 +1,34 @@
+﻿using Pokervereniging_All_In.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokervereniging_All_In.Models
+{
+    class Toernooi : Event        
+    {
+        public int Ecode { get; set; }
+        public int Mindeelnemers { get; set; }
+        public int Inleggeld { get; set; }
+        public int Eersteplaats { get; set; }
+        public int Tweedeplaats { get; set; }
+        public int Derdeplaats { get; set; }
+
+        public Toernooi(int e_code, int mindeelnemers, int inleggeld, int eersteplaats = null, int tweedeplaats = null, int derdeplaats = null )
+        {
+            Ecode = e_code;
+            Mindeelnemers = mindeelnemers;
+            Inleggeld = inleggeld;
+            Eersteplaats = eersteplaats;
+            Tweedeplaats = tweedeplaats;
+            Derdeplaats = derdeplaats;
+        }
+
+        public override string ToString()
+        {
+            return E_code.ToString();
+        }
+    }
+}
